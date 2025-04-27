0350081fcca08c7fe4419b9bb3834cafb9c191c0
jasonjiexin <13817259924@163.com>
2025-04-27 16:28:48 +0800

    东方财富证券官网爬取

---
+++ request_dome.py
@@ -0,0 +1,25 @@
+"""
+@time: 2025/4/27 下午3:58
+@author: Jason.Zhang
+@file: request_dome.py
+"""
+
+import requests
+
+# 1.指定url
+main_url = 'https://www.eastmoney.com/'
+
+# 2.发起请求
+# get函数可以根据指定的url发起网络请求
+# get函数会返回一个响应对象
+response = requests.get(url=main_url)
+
+#设置响应对象的编码格式（处理中文乱码）
+response.encoding='utf-8'
+
+# 3.获取响应数据
+page_text = response.text #text 是可以返回字符串形式的响应数据/爬取到的数据
+
+#4.持久化存储
+with open('source/request_dome_dongfang.html','w') as fp:
+    fp.write(page_text)
